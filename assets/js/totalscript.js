02738212e8ffb63bca3da6d275e174d6e44bb9c7
Bishal Debsharma <bishal.tech.com@gmail.com>
2022-07-27 23:05:12 +0530

    add slider

--- assets/js/totalscript.js
+++ assets/js/totalscript.js
@@ -1,3 +1,121 @@
 $(document).ready(function () {
-    
+    $(".teacher-slider").slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows: true,
+        dots: true,
+        speed: 300,
+        slidesToScroll: 1,
+        pauseOnHover: true,
+        centerMode: false,
+        centerPadding: "0px",
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 1100,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              centerPadding: "0px",
+              slidesToShow: 2,
+            },
+          },
+          {
+            breakpoint: 908,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              centerPadding: "0px",
+              slidesToShow: 2,
+            },
+          },
+          {
+            breakpoint: 650,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              centerPadding: "0px",
+              slidesToShow: 1,
+            },
+          },
+        ],
+      });
+
+
+    $(".review-slider").slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows: true,
+        dots: true,
+        speed: 300,
+        slidesToScroll: 1,
+        pauseOnHover: true,
+        centerMode: false,
+        centerPadding: "0px",
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 1100,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              centerPadding: "0px",
+              slidesToShow: 2,
+            },
+          },
+          {
+            breakpoint: 908,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              centerPadding: "0px",
+              slidesToShow: 2,
+            },
+          },
+          {
+            breakpoint: 650,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              centerPadding: "0px",
+              slidesToShow: 1,
+            },
+          },
+        ],
+      });
+    $(".sports-slider").slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows: true,
+        dots: true,
+        speed: 300,
+        slidesToScroll: 1,
+        pauseOnHover: true,
+        centerMode: false,
+        centerPadding: "0px",
+        slidesToShow: 4,
+        responsive: [
+          {
+            breakpoint: 1100,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              centerPadding: "0px",
+              slidesToShow: 3,
+            },
+          },
+          {
+            breakpoint: 908,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              centerPadding: "0px",
+              slidesToShow: 2,
+            },
+          },
+        ],
+      });
 });
